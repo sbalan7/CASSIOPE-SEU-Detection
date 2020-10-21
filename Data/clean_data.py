502f4568f043d4fbe6b89b8164a3647cc400d493
Shanmugha Balan <sbala7777@gmail.com>
2020-10-21 18:12:21 +0530

    cleaned data script

--- Data/clean_data.py
+++ Data/clean_data.py
@@ -1,14 +1,15 @@
 from datetime import datetime
 import datetime as dt
 import pandas as pd
-import numpy as np
 import time
 import os
 
 def clean_cassiope():
     data = pd.read_csv(os.path.join('Data','cassiope.txt'))
+
     data.columns = data.loc[0]
     data = data[1:]
+
     seu = data[0:22]
     nor = data[36:]
 
@@ -27,3 +28,28 @@ def clean_cassiope():
 def clean_omniweb():
     with open(os.path.join('Data', 'omni_headers.txt'), 'r') as f:
         lines = f.readlines()
+
+    data = pd.read_fwf(os.path.join('Data', 'omni_data.txt'), header=None)
+    data.columns = [line[3:30].rstrip() for line in lines[4:]]
+    
+    yyyyddd = [str(data.YEAR[i]) + str(data.DOY[i]) for i in range(len(data))]
+    data['Date'] = [datetime.strftime(datetime.strptime(yyyyddd[i], '%Y%j'), '%Y/%m/%d') for i in range(len(data))]
+    data['Date'] = pd.to_datetime(data['Date'], format='%Y/%m/%d', errors='coerce')
+    
+    data = data.drop(['YEAR', 'DOY', 'Hour'], axis=1)
+
+    return data
+
+print('Reading data...')
+tic = time.time()
+cas = clean_cassiope()
+omn = clean_omniweb()
+toc = time.time()
+print('Done in {:.3f} seconds'.format(toc-tic))
+
+df = cas.merge(omn, how='inner', on='Date')
+df['Date'] = (df['Date'] - dt.datetime(1970,1,1)).dt.total_seconds()
+print(df.columns)
+path = 'cleaned_data.csv'
+df.to_csv(path, index=False)
+print('File saved at {}'.format(path))
